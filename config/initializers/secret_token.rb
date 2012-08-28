6f4a131139eb2b59021e698bef1978829f56b2cb
Gary Lai <garylai1990@gmail.com>
2012-08-28 15:16:06 +0800

    build dreamers day

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamersday::Application.config.secret_token = '8fe94103153896ae3296481d78ba3adad8d5e44d376da94712631a9f797211ba3fef2921080904bdb6654861f4f3edefa3693eccfdc1abf51b6dbdfee709598b'
